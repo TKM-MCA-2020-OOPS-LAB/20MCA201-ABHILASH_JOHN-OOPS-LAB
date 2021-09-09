a0767fed9a98236671cf4ae61a8e459204410b23
Abhilash <abhilashjohn64@gmail.com>
2021-09-09 12:53:54 +0530

    CO5Q8

--- CO5/CO5Q8/CO5Q8.java
+++ CO5/CO5Q8/CO5Q8.java
@@ -1,3 +1,59 @@
 //Develop a program to handle Key events.
-public class CO5Q8 {
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class CO5Q8 implements KeyListener
+{
+    Label lb1, lbl2, lb;
+    TextField tf1;
+    Frame fr;
+    String s;
+    CO5Q8()
+    {
+
+        fr = new Frame("KeyEventListener Example");
+
+        lb1= new Label(" Key Events will be displayed based on the actions", Label.CENTER);
+        lbl2= new Label();
+        lb= new Label();
+
+        tf1 = new TextField(20);
+        fr.setLayout(new FlowLayout());
+
+        fr.add(lb1);
+
+        fr.add(tf1);
+
+        fr.add(lbl2);
+
+        tf1.addKeyListener(this);
+
+        fr.setSize(460,250);
+
+        fr.setVisible(true);
+    }
+
+    public void keyPressed(KeyEvent ev)
+    {
+        lbl2.setText(" Key pressed");
+    }
+
+    public void keyReleased(KeyEvent ev)
+    {
+        lbl2.setText("Released");
+    }
+
+    public void keyTyped(KeyEvent ev)
+    {
+        lbl2.setText("Key is typed");
+
+        fr.setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        new CO5Q8();
+    }
 }
